998bdeb7d4544139428c9d43039fcced116d63bc
LNayet <lucas.nayet27@gmail.com>
2018-03-28 00:53:20 +0200

    difficulty choice update

--- app/src/main/java/parisnanterre/fr/lexify/settings/SettingsActivity.java
+++ app/src/main/java/parisnanterre/fr/lexify/settings/SettingsActivity.java
@@ -1,6 +1,8 @@
 package parisnanterre.fr.lexify.settings;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.res.Configuration;
@@ -10,9 +12,8 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.CompoundButton;
 import android.widget.Spinner;
-import android.widget.Switch;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -22,28 +23,30 @@ import io.paperdb.Paper;
 import parisnanterre.fr.lexify.R;
 import parisnanterre.fr.lexify.application.MainActivity;
 
+import static parisnanterre.fr.lexify.verbalgame.VerbalGameFragment.millisCountDownTimer;
+
 public class SettingsActivity extends Activity implements AdapterView.OnItemSelectedListener {
 
-    private Settings settings;
-    private Switch chrono_switch;
+
     Button btn_menu;
-    public static boolean isChronoEnable = true;
     Spinner spinner_lang;
-    int spinnerPos;
+    Spinner spinner_level;
+    int spinnerPos, spinnerPos2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
 
-        chrono_switch = (Switch) findViewById(R.id.activity_settings_btn_switch);
         btn_menu = (Button) findViewById(R.id.activity_settings_submit);
-
         spinner_lang = (Spinner) findViewById(R.id.activity_languages_spinner);
         spinner_lang.setOnItemSelectedListener(this);
+        spinner_level= (Spinner) findViewById(R.id.activity_settings_spinner_level);
+        spinner_level.setOnItemSelectedListener(this);
 
         //save spinner's state
         SharedPreferences spinnersaving = getSharedPreferences("spinnerstate",0);
+        SharedPreferences spinnersaving2 = getSharedPreferences("spinnerstate",0);
 
         // Spinner Drop down elements
         List<String> categories = new ArrayList<>();
@@ -51,23 +54,27 @@ public class SettingsActivity extends Activity implements AdapterView.OnItemSele
         categories.add("English");
         categories.add("العربية");
 
-        // Creating adapter for spinner
+        List<String> categories2 = new ArrayList<>();
+        categories2.add("No Chronometer");
+        categories2.add("Easy");
+        categories2.add("Middle");
+        categories2.add("Hard");
+
+        // Creating adapters for spinners
         ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, categories);
+        ArrayAdapter<String> dataAdapter2 = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, categories2);
 
         // Drop down layout style - list view with radio button_about
         dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        dataAdapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
-        // attaching data adapter to spinner
+        // attaching data adapters to spinners
         spinner_lang.setAdapter(dataAdapter);
+        spinner_level.setAdapter(dataAdapter2);
 
         //get spinner's state
         spinner_lang.setSelection(spinnersaving.getInt("spinnerPos", 0));
-
-
-        if (isChronoEnable)
-            chrono_switch.setChecked(true);
-        else
-            chrono_switch.setChecked(false);
+        spinner_level.setSelection(spinnersaving2.getInt("spinnerPos", 0));
 
         btn_menu.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -75,21 +82,6 @@ public class SettingsActivity extends Activity implements AdapterView.OnItemSele
                 Intent i = new Intent(SettingsActivity.this, MainActivity.class);
                 startActivity(i);
             }
-        });
-
-        chrono_switch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked) {
-                    isChronoEnable = true;
-                    chrono_switch.setChecked(true);
-                } else {
-                    isChronoEnable = false;
-                    chrono_switch.setChecked(false);
-                }
-            }
-
         });
 
     }
@@ -112,7 +104,9 @@ public class SettingsActivity extends Activity implements AdapterView.OnItemSele
     @Override
     public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
         String item = adapterView.getItemAtPosition(i).toString();
+        String item2 = adapterView.getItemAtPosition(i).toString();
         spinnerPos = i;
+        spinnerPos2 = i;
         switch (item){
             case "English":
                 Paper.book().write("language", "en");
@@ -130,6 +124,25 @@ public class SettingsActivity extends Activity implements AdapterView.OnItemSele
                 spinner_lang.setPrompt("العربية");
                 break;
         }
+
+        switch (item2){
+            case "No Chronometer":
+                millisCountDownTimer=0;
+                spinner_level.setPrompt("No Chronometer");
+                break;
+            case "Easy":
+                millisCountDownTimer=40000;
+                spinner_level.setPrompt("Easy");
+                break;
+            case "Middle":
+                millisCountDownTimer=30000;
+                spinner_level.setPrompt("Middle");
+                break;
+            case "Hard":
+                millisCountDownTimer=20000;
+                spinner_level.setPrompt("Hard");
+                break;
+        }
     }
 
     @Override
@@ -140,9 +153,13 @@ public class SettingsActivity extends Activity implements AdapterView.OnItemSele
     protected void onStop(){
         super.onStop();
         SharedPreferences spinnersaving = getSharedPreferences("spinnerstate",0);
+        SharedPreferences spinnersaving2 = getSharedPreferences("spinnerstate",0);
         SharedPreferences.Editor editor = spinnersaving.edit();
+        SharedPreferences.Editor editor2 = spinnersaving2.edit();
         editor.putInt("spinnerPos", spinnerPos);
         editor.apply();
+        editor.putInt("spinnerPos", spinnerPos2);
+        editor2.apply();
     }
 
 

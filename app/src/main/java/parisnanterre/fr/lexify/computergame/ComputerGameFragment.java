e8192c52157a13e3ef0a2333736091a87b85c2b6
Nicolas Piot <piot.nicolas@outlook.Fr>
2018-10-24 11:35:21 +0200

    ajout anglais final

--- app/src/main/java/parisnanterre/fr/lexify/computergame/ComputerGameFragment.java
+++ app/src/main/java/parisnanterre/fr/lexify/computergame/ComputerGameFragment.java
@@ -165,7 +165,7 @@ public class ComputerGameFragment extends Fragment {
         determine_computer_words_and_synonymes();
         progressBar = view.findViewById(R.id.fragment_computer_game_progressbar_countdown);
 
-        round.setText("Round "+currentRound+"/4");
+        round.setText(getResources().getString(R.string.round)+" "+currentRound+"/4");
 
         if (timeSettingComputer != 0) {
             progressBar.setVisibility(View.VISIBLE);
@@ -259,14 +259,14 @@ public class ComputerGameFragment extends Fragment {
                         }
 
                         if(edittext.getText().toString().equalsIgnoreCase(ComputerWords.get(currentRound-1))) {
-                            endText.setText("Bravo vous avez gagné !");
+                            endText.setText(getResources().getString(R.string.cg_win));
                             if (timeSettingComputer!=0) {
                                 animateProgressBar.end();
                                 countDownTimer.cancel();
                             }
                         }
                         else if (currentPosition==3) {
-                            endText.setText("Vous avez perdu ! Le mot à deviner était " + ComputerWords.get(currentRound-1));
+                            endText.setText(getResources().getString(R.string.cg_lose) + ComputerWords.get(currentRound-1));
                             if (timeSettingComputer!=0) {
                                 animateProgressBar.end();
                                 countDownTimer.cancel();
@@ -326,7 +326,7 @@ public class ComputerGameFragment extends Fragment {
                     abandon.setText("Revenir menu principal");
                     next.setVisibility(View.GONE);
                 }
-                endText.setText("Vous avez perdu ! Le mot à deviner était " + ComputerWords.get(currentRound-1));
+                endText.setText(getResources().getString(R.string.cg_lose) + ComputerWords.get(currentRound-1));
                 InputMethodManager input = (InputMethodManager) getActivity().getSystemService(Activity.INPUT_METHOD_SERVICE);
                 input.hideSoftInputFromWindow(edittext.getWindowToken(), 0);
                 edittext.clearFocus();

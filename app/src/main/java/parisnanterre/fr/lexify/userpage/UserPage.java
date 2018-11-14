c04f4b4a00abb7616b8fc99081e17797dec1f508
Nicolas Piot <piot.nicolas@outlook.Fr>
2018-11-14 15:06:16 +0100

    ajout edition

--- app/src/main/java/parisnanterre/fr/lexify/userpage/UserPage.java
+++ app/src/main/java/parisnanterre/fr/lexify/userpage/UserPage.java
@@ -1,7 +1,11 @@
 package parisnanterre.fr.lexify.userpage;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.io.Serializable;
@@ -11,15 +15,64 @@ import parisnanterre.fr.lexify.database.User;
 
 public class UserPage extends Activity implements Serializable{
 
+    User u = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_page);
 
-        TextView name = findViewById(R.id.activity_user_page_name);
+        u = (User) getIntent().getSerializableExtra("user");
 
-        User u = (User) getIntent().getSerializableExtra("user");
-        int a = 5;
+        TextView name = findViewById(R.id.activity_user_page_name);
+        ImageView edit = findViewById(R.id.edit_profile);
+        TextView realName = findViewById(R.id.real_name_val);
+        TextView description = findViewById(R.id.tvDescription);
+        TextView age = findViewById(R.id.age_val);
+        TextView email = findViewById(R.id.e_mail_val);
+        TextView mobile = findViewById(R.id.mobile_val);
+
+        LinearLayout ageLayout = findViewById(R.id.age);
+        LinearLayout emailLayout = findViewById(R.id.e_mail);
+        LinearLayout mobileLayout = findViewById(R.id.mobile);
+        LinearLayout realNameLayout = findViewById(R.id.real_name);
+
+
+        realName.setText(u.getName());
+        age.setText(Integer.toString(u.getAge()));
+        email.setText(u.get_email());
+        mobile.setText(u.getMobile());
+        description.setText(u.getDescription());
+
+
+        if(realName.getText().toString().isEmpty()) {
+            realNameLayout.setVisibility(View.GONE);
+        }
+        if(age.getText().toString().isEmpty() || age.getText().equals("0")) {
+            ageLayout.setVisibility(View.GONE);
+        }
+        if(email.getText().toString().isEmpty()) {
+            emailLayout.setVisibility(View.GONE);
+        }
+        if(mobile.getText().toString().isEmpty()) {
+            mobileLayout.setVisibility(View.GONE);
+        }
+
+
+
+
+        edit.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+
+                Intent i = new Intent(getApplicationContext(), EditPage.class);
+                i.putExtra("user", u);
+                startActivity(i);
+
+            }
+
+        });
 
         name.setText(u.get_pseudo());
     }
